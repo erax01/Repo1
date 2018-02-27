c7f99ce45c3ac900293afbd34aa1f9008bb1b08d
Jordie Rieder <jrieder@student.tgm.ac.at>
2018-02-27 10:11:43 +0100

    Projekt1.java has been added

---
+++ Projekt1.java
@@ -0,0 +1,23 @@
+package rieder;
+
+import java.util.Scanner;
+
+/**
+ * Dieses Programm addiert 2 vom Benutzer eingebene ganze Zahlen
+ * @author Jordi Rieder
+ * @version 2018-02-13
+ */
+public class Projekt1 {
+	public static void main(String[] args) {
+		System.out.println(Addieren());
+	}
+	public static int Addieren() {
+		System.out.println("Bitte geben Sie die erste Zahl ein:");
+		Scanner s = new Scanner(System.in);
+		int eingabe1 = s.nextInt();
+		System.out.println("Bitte geben Sie die zweite Zahl ein:");
+		int eingabe2 = s.nextInt();
+		s.close();
+		return(eingabe1 + eingabe2);
+	}
+}
