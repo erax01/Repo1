e674eae04f484cdd3db0f2afe013faf6a689f30a
Jordie Rieder <jrieder@student.tgm.ac.at>
2018-02-27 10:24:56 +0100

    Projekt1.java has been edited

--- Projekt1.java
+++ Projekt1.java
@@ -20,4 +20,13 @@ public class Projekt1 {
 		s.close();
 		return(eingabe1 + eingabe2);
 	}
+	public static int Subtrahieren() {
+		System.out.println("Bitte geben Sie die erste Zahl ein:");
+		Scanner s = new Scanner(System.in);
+		int eingabe1 = s.nextInt();
+		System.out.println("Bitte geben Sie die zweite Zahl ein:");
+		int eingabe2 = s.nextInt();
+		s.close();
+		return(eingabe1 - eingabe2);
+	}
 }
